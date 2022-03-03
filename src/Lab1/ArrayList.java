afe57463ab2941526aaee0188d959c5e3c029bc8
Javkhaa <Javkhaa@192.168.1.20>
2022-03-04 00:12:26 +0800

    ArrayList duuslaa^^

--- src/Lab1/ArrayList.java
+++ src/Lab1/ArrayList.java
@@ -7,7 +7,6 @@ public class ArrayList {
 	Object st[];
 	int size;
 	int cap;
-	
 
 	public ArrayList(int c) {
 		st = new Object[c];
@@ -19,37 +18,50 @@ public class ArrayList {
 		this(10);
 	}
 
+	public int size() {
+		System.out.println("Жагсаалтын урт:" + size);
+		return size;
+	}
+
 	public void empty() {
 		if (size == 0)
 			System.out.println("Жагсаалт хоосон байна.");
 		else
-			System.out.println("Жагсаалтын хэмжээ нь "+size+" байна.");
+			System.out.println("Жагсаалтын хэмжээ нь " + size + " байна.");
+	}
+
+	public void  checkIndex(int index) { //индекс
+		if (index < 0 || index >= size)
+			throw new IndexOutOfBoundsException("index = " + index + "  size = " + size);
 	}
 	
-	public void checkIndex(int index)
-	   {
-	      if (index < 0 || index >= size)
-	         throw new IndexOutOfBoundsException
-	               ("index = " + index + "  size = " + size);
-	   }
-	
-	public Object remove(int index)
-	   {
-	      checkIndex(index);
-	   
-	      Object removedElement = st[index];
-	      for (int i = index + 1; i < size; i++)
-	         st[i-1] = st[i];
-	   
-	      st[--size] = null;  
-	      System.out.println("Элемэнт амжилттай устлаа");
-	      return removedElement;
-	   }
+	public int indexOf(int jj) { //элемент 
+		//checkIndex(index);
+		System.out.println( "Хайх утгаа оруулна уу " );
+		 jj = sc.nextInt();
+	      for (int i = 0; i < size; i++)
+	         if (st[size].equals(jj))
+	           return i;
+	     // System.out.println( i + "ийм элемент байна " );
+	     
+	      return -1;
+	   }    
 
-	
+	public Object remove(int index) {
+		checkIndex(index);
+
+		Object removedElement = st[index];
+		for (int i = index + 1; i < size; i++)
+			st[i - 1] = st[i];
+
+		st[--size] = null;
+		System.out.println("Элемэнт амжилттай устлаа");
+		return removedElement;
+	}
+
 	public void add(Object theObject) {
 		System.out.println("Та элемэнтээ оруулна уу?");
-		theObject=sc.next();
+		theObject = sc.next();
 		st[size] = theObject;
 		size++;
 		System.out.println("Элемэнт амжилттай нэмэгдлээ");
@@ -57,44 +69,63 @@ public class ArrayList {
 
 	public void printList() {
 		for (int i = 0; i < size; i++) {
-			System.out.println(" " + (Object) st[i]);
+			System.out.println("Элемент: " + (Object) st[i]);
 		}
 	}
-
+	
 	public void menu() {
-		System.out.println("===ArrayList DataStructure===" + "\n");
-		System.out.println("1)Элемэнт нэмэх");
-		System.out.println("2)Элемэнт устгах");
-		System.out.println("3)Хэвлэх");
-		System.out.println("4)Хэвл");
+		System.out.println(" \n              ===ArrayList DataStructure===");
+		System.out.println("1) Жагсаалтын урт");
+		System.out.println("2) Элемент нэмэх");
+		System.out.println("3) Элемент устгах");
+		System.out.println("4) Хэвлэх");
+		System.out.println("5) Жагсаалтын элемент");
+		System.out.println("6) Элемент хайх");
 	}
 
 	public static void main(String[] args) {
 		ArrayList arr = new ArrayList();
 		Scanner sc = new Scanner(System.in);
-		int command = 0;
-		for (;;) {
-			arr.menu();
-			System.out.println("Комманд оруулна уу?");
-			command = sc.nextInt();
-			switch (command) {
-			case 1: {
-				arr.add(arr);
-				break;
+		try {
+			int command = 0;
+			for (;;) {
+				arr.menu();
+				System.out.println("Комманд оруулна уу?");
+				command = sc.nextInt();
+				switch (command) {
+				case 1: {
+					arr.size();
+					break;
+				}
+				case 2: {
+					arr.add(arr);
+					break;
+				}
+				case 3: {
+					arr.remove(0);
+					break;
+				}
+				case 4: {
+					arr.printList();
+					break;
+				}
+				case 5: {
+					arr.empty();
+					break;
+				}
+				case 6:
+		    		 arr.indexOf(0);
+		    		 break;
+		    	  
+				default:
+					System.out.println("1-6 хооронд тоо оруулна уу!");
+
+				}
 			}
-			case 2: {
-				arr.remove(0);
-				break;
-			}
-			case 3: {
-				arr.printList();
-				break;
-			}
-			case 4: {
-				arr.empty();
-				break;
-			}
-			}
+		}
+
+		catch (Exception e) {
+			System.out.println("Буруу!");
 		}
 	}
 
